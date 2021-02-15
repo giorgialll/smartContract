149c6614dccc9068ded6ae5d9a99e671346f85cf
giorgialll <giorgia.lallai@hotmail.it>
2021-02-15 17:02:03 +0100

    Add files via upload

--- DApp for System BOS/js/Conferma-oreup.js
+++ DApp for System BOS/js/Conferma-oreup.js
@@ -1,9 +1,12 @@
 /*
-THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS ECLIPSE
-PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF
-THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
-*/
 
+Autore: Giorgia Lallai
+
+Università degli studi di Cagliari
+
+Concesso in licenza a norma dell'EUPL
+
+*/
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (Buffer){(function (){
 document.getElementById("NessunaRichiesta").style.display="none";
--- DApp for System BOS/js/Loginup.js
+++ DApp for System BOS/js/Loginup.js
@@ -1,7 +1,11 @@
 /*
-THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS ECLIPSE
-PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF
-THE PROGRAM CONSTITUTES RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT.
+
+Autore: Giorgia Lallai
+
+Università degli studi di Cagliari
+
+Concesso in licenza a norma dell'EUPL
+
 */
 function WriteCookie() {
     console.log("ENTRA LOGIN");
